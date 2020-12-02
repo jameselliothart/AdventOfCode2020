d07b3a101c85240aa3296e5fa81dcf5cf01e7373
James Hart <james.elliot.hart@gmail.com>
2020-12-01 20:06:15 -0600

    with recursive functions

--- Fs/App/Day1.fs
+++ Fs/App/Day1.fs
@@ -2,7 +2,28 @@ module Day1
 
 let sample = [|1721; 979; 366; 299; 675; 1456|]
 
-let productOfTwo2020Entries (entries: int array) =
+let rec recProductOfEntries condition product entries =
+    match entries with
+    | [] -> Seq.empty
+    | head :: tail ->
+        let candidate =
+            tail
+            |> Seq.map (fun x -> if (condition head x) then Some(product head x) else None)
+            |> Seq.filter Option.isSome
+        if Seq.isEmpty candidate then recProductOfEntries condition product tail
+        else candidate
+
+let recProductOfTwo2020Entries = recProductOfEntries (fun x y -> x + y = 2020) ( * )
+
+let rec recProductOfThree2020Entries entries =
+    match entries with
+    | [] -> Seq.empty
+    | head :: tail ->
+        let candidate = recProductOfEntries (fun x y -> head + x + y = 2020) (fun x y -> head * x * y) tail
+        if Seq.isEmpty candidate then recProductOfThree2020Entries tail
+        else candidate
+
+let productOfTwo2020Entries (entries: int []) =
     seq {
         for i,a in Array.indexed entries do
             for b in entries.[i..] do
