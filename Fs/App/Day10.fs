9922c5123ceaeeae0e44f52330f147e401a15638
James Hart <james.elliot.hart@gmail.com>
2020-12-10 15:02:47 -0600

    refactor

--- Fs/App/Day10.fs
+++ Fs/App/Day10.fs
@@ -1,5 +1,16 @@
 module Day10
 
+(*
+(0), 1, 4, 5, 6, 7, 10, 11, 12, 15, 16, 19, (22)
+(0), 1, 4, 6, 7, 10, 11, 12, 15, 16, 19, (22)       skip 5
+(0), 1, 4, 7, 10, 11, 12, 15, 16, 19, (22)          skip 5, 6
+(0), 1, 4, 7, 10, 12, 15, 16, 19, (22)              skip 5, 6, 11
+(0), 1, 4, 6, 7, 10, 12, 15, 16, 19, (22)           skip 5, 11
+(0), 1, 4, 5, 7, 10, 11, 12, 15, 16, 19, (22)       skip 6
+(0), 1, 4, 5, 7, 10, 12, 15, 16, 19, (22)           skip 6, 11
+(0), 1, 4, 5, 6, 7, 10, 12, 15, 16, 19, (22)        skip 11
+*)
+
 let sample1 = [|
     "16"
     "10"
@@ -53,10 +64,14 @@ let productOf joltDifferences =
     let threes = joltDifferences |> Array.find (fun (k, _) -> k = 3)
     (snd ones) * (snd threes)
 
-let solve1 (data: string []) =
+let toAdaptersAndOutlets data =
     let sorted = data |> Array.map int |> Array.sort
     let adapters = [|(Array.last sorted) + 3|] |> Array.append sorted
     let outlets = sorted |> Array.append [|0|]
+    adapters, outlets
+
+let solve1 (data: string []) =
+    let adapters, outlets = data |> toAdaptersAndOutlets
     outlets
     |> Array.map2 (-) adapters
     |> Array.groupBy id
